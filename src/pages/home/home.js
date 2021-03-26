0e1b012721a5af140f4e4014bc9229c24b6978c3
maxushka <max.beletsky21@gmail.com>
2021-03-26 22:13:47 +0300

    Change chart

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ChartPrm from '../../ChartPrm.js'
 import UPlot from '../../UPlot.js'
 import Button from 'devextreme-react/button';
+import LCharts from '../../LCharts.js'
 import './home.scss';
 
 export default () => (
@@ -9,7 +10,8 @@ export default () => (
     <h2 className={'content-block'}>Home</h2>
     <div className={'content-block'}>
       <div className={'dx-card responsive-paddings'}>
-        <UPlot/>
+        
+        <LCharts/>
       </div>
     </div>
   </React.Fragment>
